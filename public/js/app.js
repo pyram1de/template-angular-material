8ab5cd8b9dd47df66b07050d127f9bb9d925e0ce
David Hulmes <hulmesd@usshq.co.uk>
2017-02-10 17:24:28 +0000

    latest edits

--- public/js/app.js
+++ public/js/app.js
@@ -18,16 +18,16 @@
         }])
         .config(function($mdThemingProvider) {
             $mdThemingProvider.theme('default')
-            .primaryPalette('indigo', {
-            'default': '400', // by default use shade 400 from the pink palette for primary intentions
+            .primaryPalette('grey', {
+            'default': '900', // by default use shade 400 from the pink palette for primary intentions
             'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
             'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
             'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
         })
         .accentPalette('green')
         .backgroundPalette('grey',{
-            'default': '200'
-        }).dark();
+            'default': '600'
+        });
             $mdThemingProvider.theme('default-grey').backgroundPalette('grey');
             $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
             $mdThemingProvider.theme('default-orange')
