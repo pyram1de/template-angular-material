5fe64f0d403815aba812614894797a31348643e3
David Hulmes <hulmesd@usshq.co.uk>
2017-03-07 13:25:29 +0000

    with references

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
-var express = require('express');
-var app = express();
 var fs = require('fs');
 var targetDir = 'e:\\temp\\testing\\';
 var sourceDir = 'z:\\';
+var referencesFile = 'e:\\temp\\referencesFile.csv';
 var newLine = '\r\n';
 var fir=0,sec=0,las = 0;
-var filesToSkim = {};
-var FileToSkim = function(source, target) {
-    var s = source;
-    var t = target;
-    return {
-        source: s,
-        target: t
-    };
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/refereces';
+var isConnected = false;
+var db = {};
+MongoClient.connect(url, function(err, theDB){
+    db = theDB;
+    console.log('connected to db');
+});
+
+var writeReferenceFile = function(referenceType, line){    
+        var collection;
+        if(referenceType==='folder'){
+            collection = db.collection('folder');
+        } else if (referenceType==='paylocation'){
+            collection = db.collection('paylocation');
+        } else if (referenceType==='person'){ 
+            collection = db.collection('person');
+        } else {
+            fs.appendFile(referencesFile, 'N/A' + ',' + referenceType + ',' + line + '\r\n');
+            return;
+        }
+        collection.findOne({
+            "_id": Number(line[0],10)
+        }, function(err, data){
+            if(!err){                
+                if(data){
+                    fs.appendFile(referencesFile, data.reference + ',' + referenceType + ',' + line + '\r\n');
+                } else {
+                    fs.appendFile(referencesFile, 'null' + ',' + referenceType + ',' + line + '\r\n');
+                }
+            }            
+            else {
+                console.log(err);
+            }
+        });            
 }
 
 fs.readdir(sourceDir, function(err, files){   
@@ -41,9 +67,22 @@ fs.readdir(sourceDir, function(err, files){
                             var lineData = theCSVbyRow[incd].split(',');
                             var fileName = lineData[1];
                             fs.appendFileSync(targetDir+calcDir+'\\'+theCSVLocation,lineData+newLine,'utf8');
-                            if(fileName){                                
-                                var file = fs.readFileSync(sourceDir+calcDir+'\\'+fileName);
-                                fs.writeFileSync(targetDir+calcDir+'\\'+fileName,file);
+                            
+                            if(fileName){   
+                                if(theCSVLocation.indexOf('UPMPERSON')>0){
+                                    writeReferenceFile('person', lineData);
+                                } else if(theCSVLocation.indexOf('UPMFOLDER')>0){
+                                    writeReferenceFile('folder', lineData);
+                                } else if(theCSVLocation.indexOf('UPMPAYLOCATION')>0){
+                                    writeReferenceFile('paylocation', lineData);
+                                } else {
+                                    writeReferenceFile(null, lineData);
+                                }
+
+                                //var file = fs.readFileSync(sourceDir+calcDir+'\\'+fileName);
+                                //fs.writeFileSync(targetDir+calcDir+'\\'+fileName,file);
+
+
                                 /*
                                 fs.createReadStream(
                                     sourceDir+calcDir+'\\'+fileName
@@ -67,43 +106,43 @@ fs.readdir(sourceDir, function(err, files){
     }            
     });   
     
-});                
+});         
 
-                /*
-            for(var inca = 0; inca < calcDirFiles.length-1;inca++){
-                var csvFile = calcDirFiles[inca];
-                if(csvFile&&csvFile.substr(-4)==='.csv'){                        
-                    fir+=1;
-                    console.log(fir + ' : ' + sec + ' : ' + las);
-                    // this is the csv file that we now need to look inside,
-                    // get some sample data and save.
-                    var csvData = fs.readFileSync(sourceDir+calcDir+'\\'+csvFile, 'utf8');
-                    sec+=1;
-                    console.log(fir + ' : ' + sec + ' : ' + las);
-                    fs.mkdirSync(targetDir+calcDir);
-                    // we are going to ignore errors.
-                    var splitData = csvData.split(newLine);
-                    for(var i = 0;i < (splitData.length - 1) && i < 100;i++){
-                        las+=1;
-                        console.log(fir + ' : ' + sec + ' : ' + las);
-                        var lineData = splitData[i].split(',');
-                        var fileName = lineData[1];
-                        fs.appendFileSync(targetDir+calcDir+'\\'+csvFile,lineData+newLine,'utf8');
-                        if(fileName){
-                        fs.createReadStream(
-                            sourceDir+calcDir+'\\'+fileName
-                        ).pipe(fs.createWriteStream(
-                            targetDir+calcDir+'\\'+fileName
-                        ));
-                        } else {
-                            console.log('NO filename');
-                            console.log(csvData);
-                            console.log(calcDir)
-                        }
-                    }                                        
-                }
+    /*
+for(var inca = 0; inca < calcDirFiles.length-1;inca++){
+    var csvFile = calcDirFiles[inca];
+    if(csvFile&&csvFile.substr(-4)==='.csv'){                        
+        fir+=1;
+        console.log(fir + ' : ' + sec + ' : ' + las);
+        // this is the csv file that we now need to look inside,
+        // get some sample data and save.
+        var csvData = fs.readFileSync(sourceDir+calcDir+'\\'+csvFile, 'utf8');
+        sec+=1;
+        console.log(fir + ' : ' + sec + ' : ' + las);
+        fs.mkdirSync(targetDir+calcDir);
+        // we are going to ignore errors.
+        var splitData = csvData.split(newLine);
+        for(var i = 0;i < (splitData.length - 1) && i < 100;i++){
+            las+=1;
+            console.log(fir + ' : ' + sec + ' : ' + las);
+            var lineData = splitData[i].split(',');
+            var fileName = lineData[1];
+            fs.appendFileSync(targetDir+calcDir+'\\'+csvFile,lineData+newLine,'utf8');
+            if(fileName){
+            fs.createReadStream(
+                sourceDir+calcDir+'\\'+fileName
+            ).pipe(fs.createWriteStream(
+                targetDir+calcDir+'\\'+fileName
+            ));
+            } else {
+                console.log('NO filename');
+                console.log(csvData);
+                console.log(calcDir)
             }
-            */      
+        }                                        
+    }
+}
+*/      
 
 
 
